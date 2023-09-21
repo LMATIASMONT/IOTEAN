fa0d20030c4ae89b194437bb24c5e005088e2837
LMATIASMONT <91168867+LMATIASMONT@users.noreply.github.com>
2023-09-20 22:11:16 -0500

    EAN

---
+++ humedad.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>EAN</title>
+</head>
+<body>
+    <header>
+        <h1>Grupo 1</h1>
+    </header>
+    <main>
+        <article class="post">
+            <img src="images/image_1.jpg" alt="Imagen del artículo">
+            <h2>Sensor de humedad de la tierra	</h2>
+            <p>¡Hola a todos! Hoy les traigo una reseña sobre el Arduino Uno y cómo usarlo con el Sensor de Humedad de la Tierra. Olvídate de los tecnicismos, vamos directo al grano.<br><br>
+
+			<strong>Materiales que necesitas:</strong><br><br>
+
+ - Arduino Uno<br>
+ - Sensor de Humedad de la Tierra<br>
+ - Cables jumper macho-hembra<br>
+ - Una computadora con puerto USB<br>
+ - Paso a paso para armarlo:<br><br>
+
+Conecta el Sensor de Humedad de la Tierra al Arduino Uno usando los cables jumper. Asegúrate de seguir las instrucciones del fabricante para la conexión correcta.<br><br>
+
+Conecta el Arduino Uno a tu computadora mediante un cable USB.<br><br>
+
+Descarga e instala el software Arduino IDE en tu computadora desde <a href="https://www.arduino.cc/en/software" target="_blank"> https://www.arduino.cc/en/software.</a><br><br>
+
+Abre el Arduino IDE y selecciona el modelo de tu Arduino Uno en la pestaña "Herramientas > Placa".<br><br>
+
+En la misma pestaña, selecciona el puerto COM al que está conectado tu Arduino Uno.<br><br>
+
+Ahora, ve a "Archivo > Ejemplos > Sensores > DHT" y abre el ejemplo de "DHTtester". Este es un código de ejemplo para probar el Sensor de Humedad de la Tierra.<br><br>
+
+Verás el código en la ventana del Arduino IDE. Compila el código haciendo clic en el icono de verificación.<br><br>
+
+Si la compilación es exitosa, carga el código en tu Arduino Uno haciendo clic en el icono de flecha derecha.<br><br>
+
+Abre la consola serie en el Arduino IDE para ver la lectura de humedad del sensor.<br><br>
+
+¡Listo! Ahora puedes ver la lectura de humedad de la tierra en la consola serie. Este es solo el comienzo, ya que puedes personalizar y expandir tus proyectos a partir de aquí.<br><br>
+
+<strong>¿Dónde encontrar más tutoriales?</strong><br><br>
+
+Si deseas explorar aún más posibilidades con tu Arduino Uno y el Sensor de Humedad de la Tierra, te recomiendo visitar sitios web de tutoriales como <a href="https://www.tinkercad.com/circuits/" target="_blank">https://www.tinkercad.com/circuits/</a> y https://www.instructables.com/. En estos sitios, encontrarás una gran cantidad de proyectos paso a paso y consejos útiles.</p>
+        </article>
+        <a href="index.php" class="back-button">Volver</a>
+    </main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Elen - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+	<div id="colorlib-page">
+		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+		<aside id="colorlib-aside" role="complementary" class="js-fullheight text-center">
+			<h1 id="colorlib-logo"><a href="index.html">Grupo1<span>.</span></a></h1>
+			<nav id="colorlib-main-menu" role="navigation">
+				<ul>
+					<li class="colorlib-active"><a href="index.php">Blog</a></li>
+					<li><a href="about.php">Integrantes</a></li>
+				</ul>
+			</nav>
+
+			<div class="colorlib-footer">
+				<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+			  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved 
+				<ul>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+					<li><a href="#"><i class="icon-linkedin"></i></a></li>
+				</ul>
+			</div>
+		</aside> <!-- END COLORLIB-ASIDE -->
+		<div id="colorlib-main">
+			<div class="hero-wrap js-fullheight" style="background-image: url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">
+				<div class="overlay"></div>
+				<div class="js-fullheight d-flex justify-content-center align-items-center">
+					<div class="col-md-8 text text-center">
+						<div class="img mb-4" style="background-image: url(images/IOT.jpg);"></div>
+						<div class="desc">
+							<h2 class="subheading">Universidad EAN</h2>
+							<h1 class="mb-4">IOT - Internet de las cosas</h1>
+							<p class="mb-4">El IoT es una red de dispositivos conectados que recopilan y comparten datos a través de Internet. Se utiliza en hogares, industrias y más para automatizar tareas y mejorar la eficiencia, aunque plantea desafíos de seguridad y privacidad.</p>
+							<p><a href="http://audentia-gestion.fr/cisco/IoT/internet-of-things-iot-ibsg.pdf" class="btn-custom" target="_blank">Más información<span class="ion-ios-arrow-forward"></span></a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<section class="ftco-section">
+    	<div class="container">
+    		<div class="row justify-content-center mb-5 pb-2">
+          <div class="col-md-7 heading-section text-center ftco-animate">
+            <h2 class="mb-4">BLOGS</h2>
+            <p>Un pequeño espacio donde aprenderas a darle usos a tu Arduino UNO...</p>
+          </div>
+        </div>
+    		<div class="row">
+    			<div class="col-md-4">
+    				<div class="blog-entry ftco-animate">
+							<a href="humedad.php" class="img img-2" style="background-image: url(images/image_1.jpg);"></a>
+							<div class="text text-2 pt-2 mt-3">
+								<span class="category mb-3 d-block"><a>Technology</a></span>
+	              <h3 class="mb-4"><a href="#">Sensor de humedad de la tierra - Arduino Uno</a></h3>
+	              <div class="author mb-4 d-flex align-items-center">
+	            		<a href="#" class="img" style="background-image: url(images/person_2.jpg);"></a>
+	            		<div class="ml-3 info">
+	            			<span>Written by</span>
+	            			<h3><a href="https://wa.me/573183496907">Felipe Matias</a>, <span>Sep. 20, 2023</span></h3>
+	            		</div>
+	            	</div>
+	              <div class="meta-wrap align-items-center">
+	              	<div class="half order-md-last">
+		              	<p class="meta">
+		              		<span><i class="icon-heart"></i>3</span>
+		              		<span><i class="icon-eye"></i>100</span>
+		              		<span><i class="icon-comment"></i>5</span>
+		              	</p>
+	              	</div>
+	              	<div class="half">
+		              	<p><a href="humedad.php" class="btn py-2">Continue Reading <span class="ion-ios-arrow-forward"></span></a></p>
+	              	</div>
+	              </div>
+	            </div>
+						</div>
+    			</div>
+    			<div class="col-md-4">
+    				<div class="blog-entry ftco-animate">
+							<a href="#" class="img img-2" style="background-image: url(images/image_2.jpg);"></a>
+							<div class="text text-2 pt-2 mt-3">
+								<span class="category mb-3 d-block"><a>Technology</a></span>
+	              <h3 class="mb-4"><a href="#">Sensor de lluvia - Arduino Uno</a></h3>
+	              <div class="author mb-4 d-flex align-items-center">
+	            		<a href="#" class="img" style="background-image: url(images/person_1.jpg);"></a>
+	            		<div class="ml-3 info">
+	            			<span>Written by</span>
+	            			<h3><a href="#">Jesus Torres</a>, <span>Sep. 20, 2023</span></h3>
+	            		</div>
+	            	</div>
+	              <div class="meta-wrap align-items-center">
+	              	<div class="half order-md-last">
+		              	<p class="meta">
+		              		<span><i class="icon-heart"></i>3</span>
+		              		<span><i class="icon-eye"></i>100</span>
+		              		<span><i class="icon-comment"></i>5</span>
+		              	</p>
+	              	</div>
+	              	<div class="half">
+		              	<p><a href="#" class="btn py-2">Continue Reading <span class="ion-ios-arrow-forward"></span></a></p>
+	              	</div>
+	              </div>
+	            </div>
+						</div>
+    			</div>
+    			<div class="col-md-4">
+    				<div class="blog-entry ftco-animate">
+							<a href="#" class="img img-2" style="background-image: url(images/image_3.jpg);"></a>
+							<div class="text text-2 pt-2 mt-3">
+							<span class="category mb-3 d-block"><a>Technology</a></span>
+	              <h3 class="mb-4"><a href="#">Sensor de vibración - Arduino Uno</a></h3>
+	              <div class="author mb-4 d-flex align-items-center">
+	            		<a href="#" class="img" style="background-image: url(images/person_3.jpg);"></a>
+	            		<div class="ml-3 info">
+	            			<span>Written by</span>
+	            			<h3><a href="#">Jesus Torres</a>, <span>Sep. 20, 2023</span></h3>
+	            		</div>
+	            	</div>
+	              <div class="meta-wrap align-items-center">
+	              	<div class="half order-md-last">
+		              	<p class="meta">
+		              		<span><i class="icon-heart"></i>3</span>
+		              		<span><i class="icon-eye"></i>100</span>
+		              		<span><i class="icon-comment"></i>5</span>
+		              	</p>
+	              	</div>
+	              	<div class="half">
+		              	<p><a href="#" class="btn py-2">Continue Reading <span class="ion-ios-arrow-forward"></span></a></p>
+	              	</div>
+	              </div>
+	            </div>
+						</div>
+    			</div>
+    			<div class="col-md-4">
+    				<div class="blog-entry ftco-animate">
+							<a href="#" class="img img-2" style="background-image: url(images/image_4.jpg);"></a>
+							<div class="text text-2 pt-2 mt-3">
+								<span class="category mb-3 d-block"><a>Technology</a></span>
+	              <h3 class="mb-4"><a href="#">Sensor de calidad de aire - Arduino Uno</a></h3>
+	              <div class="author mb-4 d-flex align-items-center">
+	            		<a href="#" class="img" style="background-image: url(images/person_1.jpg);"></a>
+	            		<div class="ml-3 info">
+	            			<span>Written by</span>
+	            			<h3><a href="#">Jesus Torres</a>, <span>Sep. 20, 2023</span></h3>
+	            		</div>
+	            	</div>
+	              <div class="meta-wrap align-items-center">
+	              	<div class="half order-md-last">
+		              	<p class="meta">
+		              		<span><i class="icon-heart"></i>3</span>
+		              		<span><i class="icon-eye"></i>100</span>
+		              		<span><i class="icon-comment"></i>5</span>
+		              	</p>
+	              	</div>
+	              	<div class="half">
+		              	<p><a href="#" class="btn py-2">Continue Reading <span class="ion-ios-arrow-forward"></span></a></p>
+	              	</div>
+	              </div>
+	            </div>
+						</div>
+    			</div>
+    			<div class="col-md-4">
+    				<div class="blog-entry ftco-animate">
+							<a href="#" class="img img-2" style="background-image: url(images/image_5.jpg);"></a>
+							<div class="text text-2 pt-2 mt-3">
+								<span class="category mb-3 d-block"><a>Technology</a></span>
+	              <h3 class="mb-4"><a href="#">Sensor de nivel de agua - Arduino Uno</a></h3>
+	              <div class="author mb-4 d-flex align-items-center">
+	            		<a href="#" class="img" style="background-image: url(images/person_2.jpg);"></a>
+	            		<div class="ml-3 info">
+	            			<span>Written by</span>
+	            			<h3><a href="#">Jesus Torres</a>, <span>Sep. 20, 2023</span></h3>
+	            		</div>
+	            	</div>
+	              <div class="meta-wrap align-items-center">
+	              	<div class="half order-md-last">
+		              	<p class="meta">
+		              		<span><i class="icon-heart"></i>3</span>
+		              		<span><i class="icon-eye"></i>100</span>
+		              		<span><i class="icon-comment"></i>5</span>
+		              	</p>
+	              	</div>
+	              	<div class="half">
+		              	<p><a href="#" class="btn py-2">Continue Reading <span class="ion-ios-arrow-forward"></span></a></p>
+	              	</div>
+	              </div>
+	            </div>
+						</div>
+    			</div>
+    			<div class="col-md-4">
+    				<div class="blog-entry ftco-animate">
+							<a href="#" class="img img-2" style="background-image: url(images/image_6.jpg);"></a>
+							<div class="text text-2 pt-2 mt-3">
+								<span class="category mb-3 d-block"><a>Technology</a></span>
+	              <h3 class="mb-4"><a href="#">Giroscopio, Acelerometro - Arduino Uno</a></h3>
+	              <div class="author mb-4 d-flex align-items-center">
+	            		<a href="#" class="img" style="background-image: url(images/person_3.jpg);"></a>
+	            		<div class="ml-3 info">
+	            			<span>Written by</span>
+	            			<h3><a href="#">Jesus Torres</a>, <span>Sep. 20, 2023</span></h3>
+	            		</div>
+	            	</div>
+	              <div class="meta-wrap align-items-center">
+	              	<div class="half order-md-last">
+		              	<p class="meta">
+		              		<span><i class="icon-heart"></i>3</span>
+		              		<span><i class="icon-eye"></i>100</span>
+		              		<span><i class="icon-comment"></i>5</span>
+		              	</p>
+	              	</div>
+	              	<div class="half">
+		              	<p><a href="#" class="btn py-2">Continue Reading <span class="ion-ios-arrow-forward"></span></a></p>
+	              	</div>
+	              </div>
+	            </div>
+						</div>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+		</div><!-- END COLORLIB-MAIN -->
+	</div><!-- END COLORLIB-PAGE -->
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
